b03df25f4dfa1619185f4d70d41154a490434558
KUNAL <kunalpatil3303@gmail.com>
2025-08-25 10:12:16 -0400

    Lesson 76

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 void main() {
   runApp(XylophoneApp());
@@ -7,12 +7,21 @@ void main() {
 
 class XylophoneApp extends StatelessWidget {
   XylophoneApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: SafeArea(child: Center(child: Text(adjectives.last))),
+        body: SafeArea(
+          child: Center(
+            child: TextButton(
+              onPressed: () async {
+                final player = AudioPlayer();
+                await player.play(AssetSource('note1.wav'));
+              },
+              child: Text("Click Me"),
+            ),
+          ),
+        ),
       ),
     );
   }
