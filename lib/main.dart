d441bab0986e394a64682f742725c13051db6129
KUNAL <kunalpatil3303@gmail.com>
2025-08-25 09:13:51 -0400

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+
+void main() {
+  runApp(XylophoneApp());
+}
+
+class XylophoneApp extends StatelessWidget {
+  XylophoneApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: SafeArea(child: Center(child: Text(adjectives.last))),
+      ),
+    );
+  }
+}
