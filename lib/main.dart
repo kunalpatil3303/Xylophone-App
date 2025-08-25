be0556dfc463d46ef236547b845ac06b4f77e372
KUNAL <kunalpatil3303@gmail.com>
2025-08-25 17:31:31 -0400

    Lesson 77

--- lib/main.dart
+++ lib/main.dart
@@ -7,19 +7,83 @@ void main() {
 
 class XylophoneApp extends StatelessWidget {
   XylophoneApp({super.key});
+
+  void changenote(int SoundNumber) async {
+    final player = AudioPlayer();
+    await player.play(AssetSource('note$SoundNumber.wav'));
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Center(
-            child: TextButton(
-              onPressed: () async {
-                final player = AudioPlayer();
-                await player.play(AssetSource('note1.wav'));
-              },
-              child: Text("Click Me"),
-            ),
+          child: Column(
+            children: [
+              TextButton(
+                onPressed: () async {
+                  changenote(1);
+                },
+                child: Text(
+                  "Click me to play",
+                  style: TextStyle(backgroundColor: Colors.red),
+                ),
+              ),
+              TextButton(
+                onPressed: () async {
+                  changenote(2);
+                },
+                child: Text(
+                  "Click me to play",
+                  style: TextStyle(backgroundColor: Colors.blue),
+                ),
+              ),
+              TextButton(
+                onPressed: () async {
+                  changenote(3);
+                },
+                child: Text(
+                  "Click me to play",
+                  style: TextStyle(backgroundColor: Colors.yellow),
+                ),
+              ),
+              TextButton(
+                onPressed: () async {
+                  changenote(4);
+                },
+                child: Text(
+                  "Click me to play",
+                  style: TextStyle(backgroundColor: Colors.green),
+                ),
+              ),
+              TextButton(
+                onPressed: () async {
+                  changenote(5);
+                },
+                child: Text(
+                  "Click me to play",
+                  style: TextStyle(backgroundColor: Colors.teal),
+                ),
+              ),
+              TextButton(
+                onPressed: () async {
+                  changenote(6);
+                },
+                child: Text(
+                  "Click me to play",
+                  style: TextStyle(backgroundColor: Colors.orange),
+                ),
+              ),
+              TextButton(
+                onPressed: () async {
+                  changenote(7);
+                },
+                child: Text(
+                  "Click me to play",
+                  style: TextStyle(backgroundColor: Colors.purple),
+                ),
+              ),
+            ],
           ),
         ),
       ),
